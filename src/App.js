17b2b1be54605aaaa0173f70d331d25fdfa1acda
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-05-21 16:44:25 -0600

    position

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <h1>I'm in a section</h1>
         <p>Sections are used to group together related elements</p>
       </section>
+      <script type='module' src='JS file'>I point to javascript</script>
+      <div className='box-model'>I am a Box Model</div>
+      <div className='box-rel'>Relative
+      <div className='box-absolute'>absolute-bottom 0px</div></div>
+      <div className='box-fix'>fixed</div>
+      <div className='box-sticky'>sticky</div>
     </div>
   );
 }
